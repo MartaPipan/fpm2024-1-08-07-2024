1f9b97a07096eab5fccda90ca8f7c64b7fa53466
Marta Prystatska <tradlisboa@gmail.com>
2024-07-11 21:21:26 +0100

    add useEffect in StopWatch

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -2,32 +2,31 @@ import { useState, useEffect } from "react";
 
 const StopWatch = () => {
   const [time, setTime] = useState(0);
-    const [isRunning, setIsRunning] = useState(false);
-    
-    const handleIsRunning = () => {
-        setIsRunning(!isRunning);
-    }
+  const [isRunning, setIsRunning] = useState(false);
 
-        //   if (isRunning) { did not used !!!!!!!!!!!       we used inside hooks useEffect!!!!
-     }
+  const handleIsRunning = () => {
+    setIsRunning(!isRunning);
+  };
+
+  //   if (isRunning) { did not used !!!!!!!!!!!       we used inside hooks useEffect!!!
 
   useEffect(() => {
-    console.log("add interval");
-    const IdInterval = setInterval(() => {
-      setTime((time) => time + 1);
-    }, 1000);
-    return () => {
-      clearInterval(IdInterval);
-      console.log("clear interval");
-    };
-  }, []);
-    
+    if (isRunning) {
+      console.log("add interval");
+      const IdInterval = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+      return () => {
+        clearInterval(IdInterval);
+        console.log("clear interval");
+      };
+    }
+  }, [isRunning]);
+
   return (
     <div>
-      <h2>
-        {time}
-        <button onClick={handleIsRunning}> {isRunning ? "stop" : "start"}</button>
-      </h2>
+      <h2>{time}</h2>
+      <button onClick={handleIsRunning}>{isRunning ? "stop" : "start"}</button>
     </div>
   );
 };
