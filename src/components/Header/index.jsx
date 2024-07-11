03035bbc1124927a9798dbc07f20d14ca5390ca8
Marta Prystatska <tradlisboa@gmail.com>
2024-07-12 00:43:42 +0100

    useContext  importUserContext   (method alternative for Provider+Consumer)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,32 @@
+import { useContext } from "react";
+import { UserContext } from "../../contexts";
 import NavMenu from "../NavMenu";
 
 const Header = () => {
-    return (
-        <header>
-           <NavMenu /> 
-        </header>
-    );
-}
+  const { login } = useContext(UserContext);
+  return (
+    <header>
+      <NavMenu />
+      <button>light:dark</button>
+      <span> Hi, {login}</span>
+    </header>
+  );
+};
 
 export default Header;
+
+//   <<<<<<< antes
+//const Header = () => {
+// const {login} = useContext(UserContext);
+
+//console.log(value); in console:{id: 5, login: "max", avatar: "/defava.png"}
+// value: id, login, avatar
+//value={ login } ====>>>const value = useContext(UserContext);const{login}= useContext(UserContext);
+
+//return (
+//<header>
+//  <NavMenu />
+// <span> Hi, {login}</span>
+///header>
+//);
+//};
