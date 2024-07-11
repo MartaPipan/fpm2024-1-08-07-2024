8e4c9f172b8d186f363b29ffc67c9dfe501a323e
Marta Prystatska <tradlisboa@gmail.com>
2024-07-11 13:37:47 +0100

    move handleClick for useEffect

--- src/components/Clicker/index.jsx
+++ src/components/Clicker/index.jsx
@@ -1,27 +1,24 @@
 import { useState, useEffect } from "react";
-import "./Clicker.css"
-
+import "./Clicker.css";
 
 const Clicker = () => {
-    const [clickCount, setClickCount] = useState(0);
-    const handleClick = () => {
-        setClickCount(clickCount + 1);
-        
-    };
-    
-    useEffect(() => {
-        console.log('render');
-        return () => {
-        console.log('remove effect');    
-        };
-    }, []);
+  const [clickCount, setClickCount] = useState(0);
 
-    //document.body.addEventListener('click', handleClick);
-    console.log('render');
-    return (
-        <h2 onClick={handleClick}>Clicker:{clickCount}</h2>
-    );
-}
+  useEffect(() => {
+    const handleClick = () => {
+      setClickCount(clickCount + 1);
+    };
+    console.log("add effect");
+    document.body.addEventListener("click", handleClick);
+    return () => {
+      console.log("remove effect");
+      document.body.addEventListener("click", handleClick);
+    };
+  });
+
+  console.log("render");
+  return <h2>Clicker:{clickCount}</h2>;
+};
 
 export default Clicker;
 
