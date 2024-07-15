b3e31aead6e4193d4d38aff118cbf8d52d11466d
Marta Prystatska <tradlisboa@gmail.com>
2024-07-15 13:22:54 +0100

    Lesson 81-1. BOOKS & EVENTS   add state & setState in ..... . jsx(start to work)

--- src/components/Books/index.jsx
+++ src/components/Books/index.jsx
@@ -1,11 +1,28 @@
 import { useEffect, useState } from "react";
 
 const Books = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const [books, setBooks] = useState([]);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+    useEffect(() => {
+      
+        return () => {
+        
+    };
+  }, []);
+  if (isPending) {
+    return <h3>Loading...</h3>;
+  }
+  if (error) {
+    return <h3>{error.toString()}</h3>;
+  }
+    if (books.length === 0) {
+        return <h3>books not found</h3>
+    }
+    return <section>
+      books
+  </section>;
+};
 
 export default Books;
--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -1,11 +1,28 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
 const Events = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const [events, setEvents] = useState([]);
+  const [isPending, setIsPending] = useState(false);
+    const [error, setError] = useState(null);
+    
+    useEffect(() => {
+      
+        return () => {
+        
+    };
+  }, []);
+  if (isPending) {
+    return <h3>Loading...</h3>;
+  }
+  if (error) {
+    return <h3>{error.toString()}</h3>;
+  }
+        if (events.date === 0) {
+        return <h3>events not found</h3>
+    }
+    return <section>
+      events
+  </section>;
+};
 
 export default Events;
