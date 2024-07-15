62aa0d1f2b008fff8d21377aa798fb987f9b875d
Marta Prystatska <tradlisboa@gmail.com>
2024-07-15 21:19:49 +0100

    useReducer  ==>>>used example  with ContactForm

--- src/components/forms/ContactForm/index.jsx
+++ src/components/forms/ContactForm/index.jsx
@@ -1,18 +1,28 @@
-import { useState } from "react";
+import { useReducer} from "react";
 
-const ContactForm = () => {
-  const [state, setState] = useState({
+const initialState = {
     email: "",
     phone: "",
     question: "",
-  });
+};
 
-  const handleInput = ({ target: { name, value } }) => {
-    setState({
-      ...state,
-      [name]: value,
-    });
-  };
+// const reducer = (state, action) => {return newState;};
+
+const reducer = (state, action) => {
+    const { name, value } = action;
+    const newState = { ...state, [name]: value };
+    return newState;
+};
+
+
+const ContactForm = () => {
+    const [{ email, phone, question }, dispatch] = useReducer(reducer, initialState)
+
+    const handleInput = ({ target: { name, value } }) => { 
+        const action = { name, value };
+        dispatch(action);
+    };
+  
   return (
     <div>
       <h3>Contact form</h3>
@@ -20,21 +30,21 @@ const ContactForm = () => {
         type="text"
         name="email"
         placeholder="email"
-        value={state.email}
+        value={email}
         onChange={handleInput}
       />
       <input
         type="text"
         name="phone"
         placeholder="phone"
-        value={state.phone}
+        value={phone}
         onChange={handleInput}
       />
       <input
         type="text"
         name="question"
         placeholder="question"
-        value={state.question}
+        value={question}
         onChange={handleInput}
       />
       <button type="submit">send</button>
@@ -43,3 +53,31 @@ const ContactForm = () => {
 };
 
 export default ContactForm;
+
+
+/**
+ * Reducer є важливою концепцією в функціональному програмуванні та управлінні станом додатків. У контексті бібліотек для управління станом, таких як Redux, Reducer — це функція, яка приймає поточний стан і дію (action), а потім повертає новий стан.
+
+Стебло
+Reducer — це чиста функція, яка має такий підпис: (state, action) => newState. Чиста функція означає, що вона завжди повертає один і той самий результат для тих самих вхідних даних і не має побічних ефектів.
+
+Як це працює:
+Початковий стан: Reducer зазвичай має початковий стан (initial state).
+Дія (action): Це об'єкт, який описує, що саме сталося. Зазвичай він має тип (type) і може мати додаткові дані (payload).
+Новий стан:Редуктор
+Приклад:
+
+const initialState = { count: 0 };
+
+function counterReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { count: state.count + 1 };
+        case 'DECREMENT':
+            return { count: state.count - 1 };
+        default:
+            return state;
+    }
+}
+
+ */
