bc2495800ad81abe61c105943b80c611e0b61ef4
Marta Prystatska <tradlisboa@gmail.com>
2024-07-16 14:40:42 +0100

    showMessages :  case "DATA_LOAD_SUCCESS"

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -2,23 +2,24 @@ import { useReducer, useEffect } from "react";
 
 //const reducer = (state, action) => {return newState;}
 const reducer = (state, action) => {
-    const { type, payload } = action;
-    switch (type) {
-        case "DATA_LOAD_PENDING": //we start to do action
-            break;
-        case "DATA_LOAD_SUCCESS": {  //if everything ok
-            const { users, messages } = payload;
-            const usersMap = new Map();
-            users.forEach((user) => usersMap.set(user.id, user));
-            const messagesWithAuthor = messages.map((message) => ({
-                ...message,
-                author: usersMap.get(message.authorId),
-            }));
-            return {
-                ...state,
-                users,
-                messages: messagesWithAuthor,
-            };
+  const { type, payload } = action;
+  switch (type) {
+    case "DATA_LOAD_PENDING": //we start to do action
+      break;
+    case "DATA_LOAD_SUCCESS": {
+      //if everything ok
+      const { users, messages } = payload;
+      const usersMap = new Map();
+      users.forEach((user) => usersMap.set(user.id, user));
+      const messagesWithAuthor = messages.map((message) => ({
+        ...message,
+        author: usersMap.get(message.authorId),
+      }));
+      return {
+        ...state,
+        users,
+        messages: messagesWithAuthor,
+      };
     }
 
     case "DATA_LOAD_ERROR":
@@ -26,7 +27,7 @@ const reducer = (state, action) => {
     default:
       break;
   }
-    
+
   return state;
 };
 
@@ -39,23 +40,28 @@ const Chat = () => {
   });
 
   useEffect(() => {
-    dispatch({ type: "DATA_LOAD_PENDING", payload:true });
+    dispatch({ type: "DATA_LOAD_PENDING", payload: true });
     fetch("/data/chat.json")
       .then((response) => response.json())
       .then((data) => dispatch({ type: "DATA_LOAD_SUCCESS", payload: data }))
-      .catch((error) => dispatch({ type: "DATA_LOAD_ERROR", payload: error }))
+      .catch((error) => dispatch({ type: "DATA_LOAD_ERROR", payload: error }));
   }, []);
-    const { messages, error, isPending } = state;
-    if (isPending) {
-        return <h4>Loading...</h4>
-    }
-    if (error) {
-        return <h4>Error...</h4>
-    }
+  const { messages, error, isPending } = state;
+  const showMessages = ({ id, content, author: { name } }) => (
+    <li key={id}>
+      {content}(from{name})
+    </li>
+  );
+  if (isPending) {
+    return <h4>Loading...</h4>;
+  }
+  if (error) {
+    return <h4>Error...</h4>;
+  }
   return (
     <section>
       <h2>Chat</h2>
-      {JSON.stringify(messages)}
+      {messages.length && <ul>{messages.map(showMessages)}</ul>}
     </section>
   );
 };
